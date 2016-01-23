743167bfc22be5f995483d23ed972a021883a9f8
Porras80 <PaseMarlon2709>
2016-01-23 16:20:08 -0600

    Calculadora

---
+++ Groupware.Calculadora/Groupware.Calculadora/LogicaNegocio/Acciones/Sumar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupware.Calculadora.LogicaNegocio.Acciones
+{
+    public class Sumar
+    {
+
+        public double Calcular(double operandoUno, double OperandoDos)
+        {
+            double resultado = operandoUno + OperandoDos;
+
+            return resultado;
+        }
+
+    }
+}
---
+++ Groupware.Calculadora/Groupware.Calculadora/LogicaNegocio/Especificaciones/Suma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupware.Calculadora.LogicaNegocio.Especificaciones
+{
+    class Suma
+    {
+
+        public double Calculo(double operandoUno, double OperandoDos)
+        {
+
+            double resultado;
+            //invoque a la acción correspondiente
+            Acciones.Sumar operacion = new Acciones.Sumar();
+            resultado = operacion.Calcular(operandoUno, OperandoDos);
+            return (resultado);
+
+    }
+}
